043e6d7b9d3059e88d761cf6833fc9b0ef928eda
Wonjae Lee <wonjae.lee.2@gmail.com>
2023-03-20 11:05:21 +0000

    feat: add the wrapper function

--- src/budget_optimization/core.py
+++ src/budget_optimization/core.py
@@ -1,14 +1,25 @@
 import boto3
 
 
-def get_opri_data(
+def main(
     bucket: str = "lee-budget-optimization",
     key: str = "OPRI_202303.zip",
     filename: str = "tmp/OPRI_202303.zip",
+):
+    get_opri_data(bucket, key, filename)
+
+
+def get_opri_data(
+    bucket: str,
+    key: str,
+    filename: str,
 ):
     s3 = boto3.client("s3")
     s3.download_file(bucket, key, filename)
 
 
 if __name__ == "__main__":
-    get_opri_data("lee-budget-optimization", "OPRI_202303.zip", "tmp/OPRI_202303.zip")
+    bucket: str = "lee-budget-optimization"
+    key: str = "OPRI_202303.zip"
+    filename: str = "tmp/OPRI_202303.zip"
+    main(bucket, key, filename)
